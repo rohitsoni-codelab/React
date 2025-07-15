d5a95089c3c7925be4b13b884edab6efc8a0af2b
Rohit Soni <rohitsoni3084@gmail.com>
2025-07-15 23:07:14 +0530

    inititated counter

---
+++ react-hooks/src/components/Counter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Counter.css'
+const Counter = () => {
+  return (
+    <div className="counter">
+      <p className='para'>You clicked 0 times</p>
+      <button className='btn'>Click me</button>
+    </div>
+  )
+}
+
+export default Counter
