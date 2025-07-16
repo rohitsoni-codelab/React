935b4f85ab25bf4de5d1ca9977d77d0d56ea3485
Rohit Soni <rohitsoni3084@gmail.com>
2025-07-16 13:59:06 +0530

    added hook "useState"

--- react-hooks/src/components/Counter.jsx
+++ react-hooks/src/components/Counter.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Counter.css'
 const Counter = () => {
+  const [count, setCount] = useState(0);
   return (
     <div className="counter">
-      <p className='para'>You clicked 0 times</p>
-      <button className='btn'>Click me</button>
-    </div>
+      <p className='para'>You clicked {count} times</p>
+      <button className='btn' onClick={() => { setCount(count+1)}}>Click me</button>
+    </div >
   )
 }
 
