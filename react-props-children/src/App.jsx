eeec39c018ba44e4c839abc9f7b5875df862a839
Rohit Soni <rohitsoni3084@gmail.com>
2025-07-16 19:25:26 +0530

    created fourth project

--- react-props-children/src/App.jsx
+++ react-props-children/src/App.jsx
@@ -2,14 +2,17 @@
 import './App.css'
 import Card from './Components/Card'
 function App() {
-  
+
   return (
-    <div>
+    <>
       <Card name='chotu card'>
         <p>This is the time to get seious</p>
         <p>Put your feet on the acceletor</p>
       </Card>
-    </div>
+      <div children='Hi, I am new baby'>
+        {/* <p>New paragraph</p> */}
+      </div>
+    </>
   )
 }
 
