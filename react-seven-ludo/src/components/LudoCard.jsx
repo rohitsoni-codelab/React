e468315a0be1fc65ac02c78c6ac9e8b1acdc4ede
Rohit Soni <rohitsoni3084@gmail.com>
2025-07-20 12:05:25 +0530

    used array with state

--- react-seven-ludo/src/components/LudoCard.jsx
+++ react-seven-ludo/src/components/LudoCard.jsx
@@ -3,12 +3,16 @@ import { useState } from "react"
 export default function LudoCard() {
 
     const[moves,setMoves]=useState({red:0,blue:0,yellow:0,green:0})
+    const[arr,setArr]=useState(["no moves"])
 
     function updateRed(){
         setMoves((prevMoves)=>
         {
             return {...prevMoves,red:prevMoves.red+1}
         })
+        setArr((prevArr)=>
+            [...prevArr,"->Red Moved "]
+        )
     }
 
     function updateBlue(){
@@ -16,24 +20,35 @@ export default function LudoCard() {
         setMoves((prevMoves)=>{
             return {...prevMoves,blue:prevMoves.blue+1};
         })
+
+        setArr((prevArr)=>
+            [...prevArr,"->Blue Moved "]
+        )
     }
     function updateYellow(){
         // setMoves({...moves,blue:moves.blue+1})
         setMoves((prevMoves)=>{
             return {...prevMoves,yellow:prevMoves.yellow+1};
         })
+        setArr((prevArr)=>
+            [...prevArr,"->Yellow Moved "]
+        )
     }
     function updateGreen(){
         // setMoves({...moves,blue:moves.blue+1})
         setMoves((prevMoves)=>{
             return {...prevMoves,green:prevMoves.green+1};
         })
+        setArr((prevArr)=>
+            [...prevArr,"->Green Moved "]
+        )
     }
 
     // function 
     return (
         <div>
             <p>Begin the game!</p>
+            <p>{arr}</p>
             <div className="board">
                 <p>Red Count= {moves.red}</p>
                 <button style={{backgroundColor:"red"}} onClick={updateRed}>+1</button>
