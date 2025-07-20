251b21ab35dc0fadebb933929debb636cbabd349
Rohit Soni <rohitsoni3084@gmail.com>
2025-07-20 16:58:12 +0530

    uppercase a single task

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -3,12 +3,12 @@ import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
 function App() {
-  const [arr, setArr] = useState([{value:"no task",id:uuidv4()}]); //modified
+  const [arr, setArr] = useState([{ value: "no task", id: uuidv4() }]); //modified
   const [val, setVal] = useState("");
 
   function addTaskHandler() {
-    if (val.trim() === "") return; 
-    setArr(prevArr => [...prevArr, {value:val,id:uuidv4()}]);//modified
+    if (val.trim() === "") return;
+    setArr(prevArr => [...prevArr, { value: val, id: uuidv4() }]);//modified
     setVal("");
   }
 
@@ -16,17 +16,26 @@ function App() {
     setVal(event.target.value);
   }
 
-  function deleteHandler(id){
-    setArr((prevTodo)=> prevTodo.filter((todo)=>todo.id!=id))
+  function deleteHandler(id) {
+    setArr((prevTodo) => prevTodo.filter((todo) => todo.id != id))
   }
 
-  function upperCaseHandler(){
-    setArr((prevTodo)=>(
-      prevTodo.map((todo)=>{
-        return {...todo,value:todo.value.toUpperCase()}
+  function upperCaseHandler() {
+    setArr((prevTodo) => (
+      prevTodo.map((todo) => {
+        return { ...todo, value: todo.value.toUpperCase() }
       })
     ))
   }
+
+ function upperCaseThisHandler(id) {
+  setArr((prevTodo) =>
+    prevTodo.map((todo) =>
+      todo.id === id ? { ...todo, value: todo.value.toUpperCase() } : todo
+    )
+  );
+}
+
   return (
     <div>
       <h1>Todo List</h1>
@@ -36,15 +45,19 @@ function App() {
         value={val}
         onChange={addInputHandler}
       />
-      <br />
+      <br /> <br />
       <button onClick={addTaskHandler}>Add Task</button>
 
       <ul>
         {arr.map((todos) => (
           <li key={todos.id}>
             <span>{todos.value}</span> &nbsp;&nbsp;
-            <button onClick={()=>(deleteHandler(todos.id))}>delete</button>
-            
+
+            <button onClick={() => (deleteHandler(todos.id))}>delete</button>
+
+            &nbsp;&nbsp;
+            <button onClick={()=>upperCaseThisHandler(todos.id)}>UpperCase This</button>
+
           </li>
         ))}
       </ul>
